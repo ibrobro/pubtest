1058777335580ce6dee85fd0302607b719f131cb
ibrobro <deep_doubtrix@yahoo.com>
2022-07-26 08:23:12 +0700

    saving the test code

---
+++ libs/typescript/util/src/lib/test/permutation.ts
@@ -0,0 +1,36 @@
+const arrayInput = [1,1,2];
+
+
+function permute(arr = []) {
+	let resultsAsString = [];
+  let results = [];
+
+  if(arr.length === 0) return [];
+  if(arr.length === 1) return [...arr];
+
+  for (let i = 0; i < arr.length; i++) {
+    const currentNum = arr[i];
+    const remainingArr = arr.slice(0, i).concat(arr.slice(i + 1));
+    const remainingArrPermuted = permute(remainingArr);
+
+    for (let j = 0; j < remainingArrPermuted.length; j++) {
+      const permutedArray = [currentNum].concat(remainingArrPermuted[j]);
+      const permutedArrayAsString = permutedArray.toString();
+      
+     	if (resultsAsString.indexOf(permutedArrayAsString) < 0) {
+        results.push(permutedArray);
+        resultsAsString.push(permutedArrayAsString);
+    	}
+    }
+  },
+
+  return results;
+}
+
+
+const permutedArrayOutput = permute(arrayInput);
+
+
+for (const item of permutedArrayOutput) {
+  console.log(item);
+}
